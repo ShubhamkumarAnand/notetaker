7404fae927387abf52873bc507c4105ac7b3eb49
imskanand <the.anand3099@gmail.com>
2023-02-23 09:54:02 +0530

    refactor: removed unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
 import { type NextPage } from "next";
-import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-import Link from "next/link";
 import { Header } from "~/components/Header";
-
-import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
 	return (
