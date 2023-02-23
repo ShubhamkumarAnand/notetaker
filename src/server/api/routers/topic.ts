1eaefe86fefec7870f5b9d3017f27026e4ddad19
imskanand <the.anand3099@gmail.com>
2023-02-23 09:11:49 +0530

    feat: Added trpc procedures on topic table to create and get the data

---
+++ src/server/api/routers/topic.ts
@@ -0,0 +1,22 @@
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { z } from "zod";
+
+export const topicRouter = createTRPCRouter({
+	getAll: protectedProcedure.query(({ ctx }) => {
+		return ctx.prisma.topic.findMany({
+			where: {
+				userId: ctx.session.user.id,
+			},
+		});
+	}),
+	create: protectedProcedure
+		.input(z.object({ title: z.string() }))
+		.mutation(({ ctx, input }) => {
+			return ctx.prisma.topic.create({
+				data: {
+					title: input.title,
+					userId: ctx.session.user.id,
+				},
+			});
+		}),
+});
